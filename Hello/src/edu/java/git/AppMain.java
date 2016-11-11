f85367e0c4ba57ff5e3ae18d9a1ab304b4f59fac
stu <stu@itwill>
2016-11-11 14:20:19 +0900

    세번째

--- Hello/src/edu/java/git/AppMain.java
+++ Hello/src/edu/java/git/AppMain.java
@@ -4,7 +4,7 @@ public class AppMain {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
-		System.out.println("¾È³çÇÏ¼¼¿ä~");
+		System.out.println("안녕하세요~");
 		System.out.println("Hello, world!");
 	}
 
